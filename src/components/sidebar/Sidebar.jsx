5f4e42f77b13ed389129077cbd36fe9f25a8ea51
mdnahidislam <mdnahidislam30@gmail.com>
2022-08-23 21:50:00 +0600

    Sidebar design complete.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,88 @@
 import './sidebar.scss';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import StoreIcon from '@mui/icons-material/Store';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import SettingsApplicationsOutlinedIcon from '@mui/icons-material/SettingsApplicationsOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
+import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">Dev NiN</span>
+      </div>
 
-export default Sidebar
+      <hr />
+
+      <div className="center">
+        <ul>
+          
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+
+          <p className="title">LIST</p>
+          <li>
+            <PersonOutlineIcon className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <CreditCardOutlinedIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <StoreIcon className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <LocalShippingOutlinedIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+
+          <p className="title">USEFULL</p>
+          <li>
+            <InsertChartIcon className="icon" />
+            <span>Status</span>
+          </li>
+          <li>
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <span>Notification</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon" />
+            <span>Logs</span>
+          </li>
+
+          <p className="title">USER</p>
+          <li>
+            <SettingsApplicationsOutlinedIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Prifile</span>
+          </li>
+          <li>
+            <ExitToAppOutlinedIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
