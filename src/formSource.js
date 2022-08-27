1773f2ea8aa11e91cb3acc9edacbe0d65cbe1588
mdnahidislam <mdnahidislam30@gmail.com>
2022-08-27 21:46:35 +0600

    Add new user page design complete

---
+++ src/formSource.js
@@ -0,0 +1,32 @@
+export const userInputs = [
+  {
+    id: 1,
+    label: 'Username',
+    type: 'text',
+    placeholder: 'nahidislam',
+  },
+  {
+    id: 2,
+    label: 'Name',
+    type: 'text',
+    placeholder: 'Nahid Islam',
+  },
+  {
+    id: 3,
+    label: 'Email',
+    type: 'email',
+    placeholder: 'nahidislam@gmail.com',
+  },
+  {
+    id: 4,
+    label: 'Phone',
+    type: 'number',
+    placeholder: '01788080397',
+  },
+  {
+    id: 5,
+    label: 'Password',
+    type: 'password',
+    placeholder: '******',
+  },
+];
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,11 +1,49 @@
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './new.scss';
+import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
 
-const New = () => {
+const New = ({ inputs, title }) => {
   return (
-    <div>
-      New Page
-    </div>
-  )
-}
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h3 className="title">{title}</h3>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <input type="file" id="file" style={{ display: 'none' }} />
+              </div>
 
-export default New
+              {inputs.map((input) => (
+                <div className="formInput">
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
+
+              <div className="btn">
+                <button>Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default New;
